164ed63b4efe686c3e6afc968f3c4c9b09d6a87b
Bosko Ivkovic <bosko2003414@gmail.com>
2018-06-09 13:45:03 +0200

    Fixed Lava map 60%, RESPAWN IN PROGRESS

---
+++ Assets/Scenes/HardCodedMapScripts/LavaLevel/Rotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Rotation : NetworkBehaviour {
+
+    public float rotation;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Rotate(0, 0, rotation);
+	}
+}
---
+++ Assets/_SCRIPTS/Respawn.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour {
+
+    public Vector3 respawnLoc;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Car")
+        {
+            Debug.Log("collide?");
+            //col.transform.position = new Vector3(respawnLoc.x, respawnLoc.y, respawnLoc.z);
+            col.transform.position = respawnLoc;
+        }
+    }
+
+}
